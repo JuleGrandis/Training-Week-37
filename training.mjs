6f310d44441f81e5480596ab3cd0d8f2cb6631bf
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2024-09-09 15:06:52 +0200

    Task A complete

---
+++ training.mjs
@@ -0,0 +1,7 @@
+console.log("Task A")
+
+const taskAText = "Debugging is like being a detective in a crime drama where you are also the murderer";
+
+for (let i = 0; i < 100; i++) {
+    console.log(taskAText);
+}
